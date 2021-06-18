e5e1a492affce38e9b989901b14bad5a64c28efe
rosswilsonmedia <82285604+rosswilsonmedia@users.noreply.github.com>
2021-06-18 14:14:59 -0700

    dojoSurveyValidation commit

---
+++ flask_app/controllers/dojo_controller.py
@@ -0,0 +1,21 @@
+from flask_app import app
+
+from flask import render_template, request, redirect, session, flash
+from ..models.dojo import Dojo
+
+@app.route('/')
+def index():
+    session.pop('form_response')
+    return render_template('index.html')
+@app.route('/submit', methods=['POST'])
+def submit():
+    if Dojo.validate_form(request.form):
+        Dojo.save(request.form)
+        session['form_response']=request.form
+        return redirect('/result')
+    else:
+        return redirect('/')
+
+@app.route('/result')
+def result_page():
+    return render_template('result.html', form_response=session['form_response'])
---
+++ flask_app/models/dojo.py
@@ -0,0 +1,35 @@
+from ..config.mysqlconnection import connectToMySQL
+from flask import flash
+
+class Dojo:
+    def __init__(self, data):
+        self.id=data['id']
+        self.name=data['name']
+        self.location=data['location']
+        self.language=data['language']
+        self.comment=data['comment']
+        self.created_at=data['created_at']
+        self.updated_at=data['updated_at']
+
+    @classmethod
+    def save(self, data):
+        query="INSERT INTO dojos (name, location, language, comment, created_at, updated_at)"\
+            "VALUES (%(name)s, %(location)s, %(language)s, %(comment)s, NOW(), NOW());"
+
+        return connectToMySQL('dojo_survey_schema').query_db(query, data)
+
+    @staticmethod
+    def validate_form(data):
+        is_valid=True
+
+        if len(data['name'])<2:
+            flash("Name must be at least two characters.")
+            is_valid=False
+        if data['location']==None or len(data['location'])<2:
+            flash("Location not valid for this form.")
+            is_valid=False
+        if data['language']==None or len(data['language'])<2:
+            flash("Language not valid for this form.")
+            is_valid=False
+
+        return is_valid
